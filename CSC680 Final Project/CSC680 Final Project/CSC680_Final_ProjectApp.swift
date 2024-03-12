f4d20d13d198ce5cfad19ec363dce966d3647caa
brianjien <brianjienop49792399@gmail.com>
2024-03-12 10:41:04 -0700

    initial the program

---
+++ CSC680 Final Project/CSC680 Final Project/CSC680_Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSC680_Final_ProjectApp.swift
+//  CSC680 Final Project
+//
+//  Created by brianjien on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CSC680_Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CSC680 Final Project/CSC680 Final Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CSC680 Final Project
+//
+//  Created by brianjien on 3/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
